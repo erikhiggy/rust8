b80417ba7a2d6fcae521babd419653f290ff5f11
Erik Higginbotham <erik96@me.com>
2020-12-22 09:32:09 -0500

    most of the opcodes finished

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::Read;
 use std::ops::Deref;
+use rand::Rng;
 
 fn main() {
     let mut file = File::open("data/invaders").unwrap();
@@ -29,7 +30,9 @@ struct Cpu {
     reg_i: u16,
 
     // 2 special purpose 8 bit registers
-    // reg_sp: [u8; NUM_SP],
+    // delay and sound timers
+    reg_dt: u8,
+    reg_st: u8,
 
     // 16 bit program counter
     reg_pc: u16,
@@ -48,7 +51,9 @@ impl Cpu {
             reg_i: 0,
             reg_pc: PROGRAM_START_ADDR,
             sp: 0,
-            stack: [0; 16]
+            stack: [0; 16],
+            reg_dt: 0,
+            reg_st: 0
         }
     }
 
@@ -58,6 +63,12 @@ impl Cpu {
         let lo = ram.read_byte(self.reg_pc+1) as u16;
         let instruction: u16 = (hi << 8) | lo;
         // decode and execute the opcode
+        let reg_vx = self.reg_gpr[(instruction & 0x0F00) >> 8];
+        let reg_vy = self.reg_gpr[(instruction & 0x00F0) >> 4];
+        let nnn = instruction & 0x0FFF;
+        let nn = instruction & 0x00FF;
+        let reg_v0 = self.reg_gpr[0];
+        let mut reg_vf = self.reg_gpr[0x000F];
         match instruction & 0xF000 {
             0x0000 => match instruction & 0x000F {
                 0x0000 => {
@@ -65,11 +76,15 @@ impl Cpu {
                 },
                 0x000E => {
                     // 0x00EE: return from subroutine
+                    // restores program counter and then removes stack address
+                    self.sp -= 1;
+                    self.reg_pc = self.stack[self.sp] as u16;
                 },
                 _ => println!("Invalid opcode {}", instruction)
             },
             0x1000 => {
                 // 0x1NNN: jumps to address NNN
+                self.reg_pc = nnn;
             },
             0x2000 => {
                 // 0x2NNN: calls subroutine at NNN
@@ -79,86 +94,210 @@ impl Cpu {
             },
             0x3000 => {
                 // 0x3XNN: skips the next instruction if VX === NNN
+                if reg_vx == nn {
+                    self.reg_pc += 2;
+                }
+                self.reg_pc += 2;
             },
             0x4000 => {
                 // 0x4XNN: skips the next instruction if VX !== NNN
+                if reg_vx != nn {
+                    self.reg_pc += 2;
+                }
+                self.reg_pc += 2;
             },
             0x5000 => {
                 // 0x5XY0: skips the next instruction if VX === VY
+                if reg_vx == reg_vy {
+                    self.reg_pc += 2;
+                }
+                self.reg_pc += 2;
             },
             0x6000 => {
                 // 0x6XNN: sets VX to NN
+                reg_vx = nn;
+                self.reg_pc += 2;
             },
             0x7000 => {
                 // 0x7XNN: Adds NN to VX (carry flag is not changed)
+                reg_vx += nn;
+                self.reg_pc += 2;
             },
             0x8000 => {
                 match instruction & 0x000F {
                     0x0000 => {
                         // 0x8XY0: sets VX = VY
+                        reg_vx = reg_vy;
+                        self.reg_pc += 2;
                     },
                     0x0001 => {
-                        // 0x8XY1: bitwise OR -> VX | VY
+                        // 0x8XY1: bitwise OR -> VX | VY, store in VX
+                        reg_vx |= reg_vy;
+                        self.reg_pc += 2;
                     },
                     0x0002 => {
                         // 0x8XY2: bitwise AND -> VX & VY
+                        reg_vx &= reg_vy;
+                        self.reg_pc += 2;
                     },
                     0x0003 => {
                         // 0x8XY3: XOR -> VX XOR VY
+                        reg_vx ^= reg_vy;
+                        self.reg_pc += 2;
                     },
                     0x0004 => {
-                        // 0x8XY4: sets VX = VY
+                        // 0x8XY4: adds VY to VX. VF is set to 1 when there's a carry
+                        // and a 0 when when there isn't
+                        if reg_vy > (0x00FF - reg_vx) {
+                            reg_vf = 1;
+                        } else {
+                            reg_vf = 0;
+                        }
+                        reg_vx += reg_vy;
+                        self.reg_pc += 2;
+
                     },
                     0x0005 => {
-                        // 0x8XY5: adds VY to VX. VF is set to 1 when there's a carry
+                        // 0x8XY5: subtracts VY from VX. VF is set to 1 when there's a carry
                         // and a 0 when when there isn't
+                         if reg_vx > reg_vy {
+                             reg_vf = 1;
+                         } else {
+                             reg_vf = 0;
+                         }
+                        reg_vx -= reg_vy;
+                        self.reg_pc += 2;
                     },
                     0x0006 => {
                         // 0x8XY6: stores the LSB of VX in VF and then shifts VX to the right by 1
+                        reg_vf = reg_vx & 1;
+                        reg_vx = reg_vx >> 1;
+                        self.reg_pc += 2;
                     },
                     0x0007 => {
                         // 0x8XY7: sets VX to VY minus VX.
                         // VF is set to 0 when there's a borrow, and 1 when there isn't
+                        if reg_vy > reg_vx {
+                            reg_vf = 1;
+                        } else {
+                            reg_vf = 0;
+                        }
+                        reg_vy -= reg_vx;
+                        self.reg_pc += 2;
                     },
                     0x000E => {
                         // 0x8XYE: stores the MSB of VX in VF and then shifts VX to the left by 1
+                        reg_vf = (reg_vx >> 3) & 1;
+                        reg_vx = reg_vx << 1;
+                        self.reg_pc += 2;
                     },
                     _ => println!("Invalid opcode {}", instruction)
                 }
             },
             0x9000 => {
                 // 0x9XY0: skips the next instruction if VX doesn't equal VY
+                if reg_vx != reg_vy {
+                    self.reg_pc += 2;
+                }
+                self.reg_pc += 2;
             },
             0xA000 => {
                 // 0xANNN: sets I to the address NNN
+                self.reg_i = nnn;
+                self.reg_pc += 2;
             },
             0xB000 => {
                 // 0xBNNN: jumps to the address NNN plus V0
+                self.reg_pc = nnn + reg_v0;
             },
             0xC000 => {
                 // 0xCXNN: sets VX to the result of a bitwise AND operation
                 // on a random number (Typically: 0 to 255) and NN
+                let mut rng = rand::thread_rng();
+                let rand_num: u8 = rng.gen();
+                reg_vx = rand_num & nn;
+                self.reg_pc += 2;
             },
             0xD000 => {
                 // 0xDXYN: draws a sprite at coordinate (VX, VY), has a width of 8 pixels and
                 // a height of N + 1 pixels.
-                // TODO: Figure out GFX later
+                // TODO: Figure out GFX
             },
             0xE000 => {
                 match instruction & 0x000F {
                     0x000E => {
                         // 0xEX9E: skips the next instruction if the key stored in VX is pressed
+                        // TODO: Figure out keyboard input
                     },
                     0x0001 => {
                         // 0xEXA1: skips the next instruction if the key stored in VX isn't pressed
-                    }
+                        // TODO: Figure out keyboard input
+                    },
+                    _ => println!("Invalid opcode! {}", instruction)
+                }
+            },
+            0xF000 => {
+                match instruction & 0x000F {
+                    0x0007 => {
+                        // 0xFX07: the value of DT is placed in VX
+                        reg_vx = self.reg_dt;
+                        self.reg_pc += 2;
+                    },
+                    0x000A => {
+                        // 0xFX0A: wait for key press, store the value of key into VX
+                        // TODO: Figure out keyboard input
+                    },
+                    0x0005 => {
+                        match instruction & 0x00FF {
+                            0x0015 => {
+                                // 0xFX15: set DT to VX
+                                self.reg_dt = reg_vx;
+                                self.reg_pc += 2;
+                            },
+                            0x0055 => {
+                                // 0xFX55: store registers V0 -> VX in memory starting at
+                                // location I
+                                for x in 0..=self.reg_gpr.iter().position(|&val| val == reg_vx).unwrap() {
+                                    ram.write_byte(self.reg_i + x, self.reg_gpr[x])
+                                }
+                            },
+                            0x0065 => {
+                                // 0xFX65: read registers V0 -> Vx from memory starting at
+                                // location
+                                for x in self.reg_i..=ram.memory[self.reg_gpr.iter().position(|&s| s == reg_vx).unwrap()] {
+                                    ram.read_byte(ram.memory[x])
+                                }
+                            },
+                            _ => println!("Invalid opcode! {}", instruction)
+                        }
+                    },
+                    0x0008 => {
+                        // 0xFX18: set ST to VX
+                        self.reg_st = reg_vx;
+                        self.reg_pc += 2;
+                    },
+                    0x000E => {
+                        // 0xFX1E: set I = I + VX
+                        self.reg_i += reg_vx;
+                        self.reg_pc += 2;
+                    },
+                    0x0009 => {
+                        // 0xFX29: set I = location of sprite for digit VX
+                        // TODO: Figure out GFX
+                    },
+                    0x0003 => {
+                        // 0xFX33: store BCD representation of VX in memory locations
+                        // I, I+1, I+2
+                        ram[self.reg_i] = reg_vx / 100;
+                        ram[self.reg_i + 1] = (reg_vx / 10) % 10;
+                        ram[self.reg_i + 2] = (reg_vx % 100) % 10;
+                        self.reg_pc += 2;
+                    },
+                    _ => println!("Invalid opcode! {}", instruction)
                 }
             }
             _ => println!("Invalid opcode! {}", instruction)
         }
-        // TODO: update timers
-        // increment the PC by 2 after each instruction
-        self.reg_pc += 2;
     }
 }
 
